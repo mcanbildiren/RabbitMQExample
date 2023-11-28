2e84b81126c729110cd7d29075ba19dbd182015b
Mustafa Can Bildiren <78155451+mcanbildiren@users.noreply.github.com>
2023-11-29 00:38:29 +0300

    Hello World message sent and received.

--- RabbitMQExample.Consumer/Program.cs
+++ RabbitMQExample.Consumer/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+var consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume("new-queue", true, consumer);
+
+consumer.Received += (sender, args) =>
+{
+    var message = Encoding.UTF8.GetString(args.Body.ToArray());
+
+    Console.WriteLine("Message: " + message);
+};
+
+Console.ReadLine();
--- RabbitMQExample.Publisher/Program.cs
+++ RabbitMQExample.Publisher/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.QueueDeclare("new-queue", true, false, false);
+
+string message = "Hello World!";
+
+var messageBody = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(string.Empty, "new-queue", null, messageBody);
+
+Console.WriteLine("Message sent!");
+Console.ReadLine();
